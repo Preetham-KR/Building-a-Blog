582178e455506c71cf32daa65e0465936fa27423
Preetham K R <preethamkr143@gmail.com>
2023-11-26 20:18:21 +0530

    Create our basic blog

---
+++ app/controllers/blog_posts_controller.rb
@@ -0,0 +1,55 @@
+class BlogPostsController < ApplicationController
+    before_action :authenticate_user!, except: [:index, :show]
+
+    def index 
+      @blog_posts= BlogPost.all  
+    end
+
+    def show
+        @blog_post= BlogPost.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+        redirect_to root_path
+    end
+
+    def new
+        @blog_post= BlogPost.new
+    end
+
+    def create
+        @blog_post= BlogPost.new(blog_post_params)
+        if @blog_post.save
+            redirect_to @blog_post
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end 
+
+    def edit
+        @blog_post = BlogPost.find(params[:id])
+    end
+
+    def update
+       @blog_post =BlogPost.find(params[:id]) 
+       if @blog_post.update(blog_post_params)
+            redirect_to @blog_post
+       else
+        render :edit, status: :unprocessable_entity
+       end
+    end
+
+    def destroy
+      @blog_post= BlogPost.find(params[:id])  
+      @blog_post.destroy
+      redirect_to root_path
+    end
+    
+    private
+    def blog_post_params
+        params.required(:blog_post).permit(:title, :body)
+    end
+
+    # def authenticate_user!
+    #     redirect_to_new_user_session_path, alert: "You need to sign in or sign up before continuing." unless user_signed_in?
+    # end
+    # Internally rails write like this 
+end
